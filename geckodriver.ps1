5ec357f57cbb866f303bdbfcd10967d1b0f316c4
Janos Szenfner <szenfner@outlook.com>
2017-05-04 16:18:45 +0200

    Initial Commit Geckodriver killer

---
+++ geckodriver.ps1
@@ -0,0 +1,14 @@
+﻿# PowerShell Kill Process
+Clear-Host
+# get Geckodriver process
+$process = Get-Process geckodriver -ErrorAction SilentlyContinue
+if ($process) {
+  # try gracefully first
+  $process.CloseMainWindow()
+  # kill after five seconds
+  Start-Sleep [-Seconds] 3
+  if (!$process.HasExited) {
+    $process | Stop-Process -Force
+  }
+}
+Remove-Variable process
---
+++ readme.md
@@ -0,0 +1,8 @@
+You can find Powershell scripts within this repository
+
+Used frameworks, libraries and technologies:
+    - Powershell
+
+Repository files:
+
+- geckodriver.ps1 : Kill the geckodrivers. It also waits 5 seconds before kills it.
